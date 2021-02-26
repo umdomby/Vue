6490a085874a249d3c61179cc4bad3191d7690fa
Navatar <umdomby@yandex.ru>
2021-02-27 00:47:14 +0300

    java+Vue start

---
+++ src/test/java/navatar/vue/VueApplicationTests.java
@@ -0,0 +1,13 @@
+package navatar.vue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
